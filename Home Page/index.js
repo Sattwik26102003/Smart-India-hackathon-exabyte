0ad059837cd2385efce6895d0f3ad8b075090424
Subham Mahato <sbmmahato@gmail.com>
2023-09-27 16:26:04 +0530

    big change

---
+++ Home Page/index.js
@@ -0,0 +1,61 @@
+
+document.querySelectorAll("#pop-click")[0].addEventListener("click",function(){
+document.querySelector(".popup").classList.add("active");
+});
+document.querySelectorAll("#pop-click")[1].addEventListener("click",function(){
+    document.querySelector(".popup").classList.add("active");
+    });
+document.querySelectorAll("#pop-click")[2].addEventListener("click",function(){
+document.querySelector(".popup").classList.add("active");
+});
+document.querySelectorAll("#pop-click")[3].addEventListener("click",function(){
+    document.querySelector(".popup").classList.add("active");
+    });
+document.querySelectorAll("#pop-click")[4].addEventListener("click",function(){
+document.querySelector(".popup").classList.add("active");
+});
+document.querySelectorAll("#pop-click")[5].addEventListener("click",function(){
+    document.querySelector(".popup").classList.add("active");
+    });
+document.querySelectorAll("#pop-click")[6].addEventListener("click",function(){
+document.querySelector(".popup").classList.add("active");
+});
+
+
+document.querySelectorAll("#pop-click")[0].addEventListener("click",function(){
+    document.querySelector("body").classList.add("active");
+    });
+    document.querySelectorAll("#pop-click")[1].addEventListener("click",function(){
+        document.querySelector("body").classList.add("active");
+        });
+    document.querySelectorAll("#pop-click")[2].addEventListener("click",function(){
+    document.querySelector("body").classList.add("active");
+    });
+    document.querySelectorAll("#pop-click")[3].addEventListener("click",function(){
+        document.querySelector("body").classList.add("active");
+        });
+    document.querySelectorAll("#pop-click")[4].addEventListener("click",function(){
+    document.querySelector("body").classList.add("active");
+    });
+    document.querySelectorAll("#pop-click")[5].addEventListener("click",function(){
+        document.querySelector("body").classList.add("active");
+        });
+    document.querySelectorAll("#pop-click")[6].addEventListener("click",function(){
+    document.querySelector("body").classList.add("active");
+    });
+
+
+
+document.querySelector(".cls-btn").addEventListener("click",function(){
+        document.querySelector("body").classList.remove("active");
+        });
+
+
+
+
+
+document.querySelector(".cls-btn").addEventListener("click",function(){
+    document.querySelector(".popup").classList.remove("active");
+    });
+    
+    
